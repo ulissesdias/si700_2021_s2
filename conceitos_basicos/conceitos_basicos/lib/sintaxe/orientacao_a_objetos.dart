17fc0deaf661c1b7c54dad67b24bb3823222df9e
Ulisses Dias <ulisses@ft.unicamp.br>
2021-08-19 23:02:42 -0300

    Último commit da segunda aula

--- conceitos_basicos/conceitos_basicos/lib/sintaxe/orientacao_a_objetos.dart
+++ conceitos_basicos/conceitos_basicos/lib/sintaxe/orientacao_a_objetos.dart
@@ -1,8 +1,8 @@
 class Professor {
   // Atributos
-  String _nome;
-  String sobrenome;
-  int idade;
+  String _nome = "";
+  String sobrenome = "";
+  int idade = 12;
 
   // Getter
   String get nome {
@@ -29,14 +29,14 @@ class Professor {
     this.idade = 38;
   }*/
 
+  
   // Sem nome, com parâmetro
-  Professor(n, sn) {
+  Professor(this._nome, this.sobrenome) {
     print("Invoquei o Construtor");
-    this._nome = n;
-    this.sobrenome = sn;
     this.idade = 25;
   }
 
+  
   // Com nome
   Professor.meuProprioConstrutor(n, sn) {
     this._nome = n;
@@ -45,8 +45,48 @@ class Professor {
   }
 }
 
+class ProfessorEmergencial extends Professor {
+
+  ProfessorEmergencial(n, sn) : super.meuProprioConstrutor(n, sn) {
+    print("Invoquei construtor na classe filha");
+  }
+
+  String displayName(){
+    return "$_nome $sobrenome";
+  }
+
+  static String getVinculo(){
+    return "Unicamp";
+  }
+}
+
+abstract class Pessoa {
+  metodoAbstrato();
+
+  metodoConcreto(id){
+    // Corpo do método
+  }
+}
+
+class PessoaHerdada extends Pessoa{
+  metodoAbstrato(){
+    print("O método abstrato recebeu corpo na classe concreta");
+  }
+}
+
+class PessoaImplementada implements Pessoa {
+  metodoAbstrato(){
+    print("Um método abstrato sendo concretizado");
+  }
+
+  metodoConcreto(id){
+    print("Tive que implementar novamente o método concreto");
+  }
+  
+}
+
 void main() {
-  Professor p1 = new Professor("Letícia", "Lewartoski");
+  Professor p1 = new Professor("Letícia", "Lewardosky");
   print(p1.sobrenome);
   print(p1.idade);
   print(p1._nome);
@@ -58,4 +98,16 @@ void main() {
   print("");
   Professor p2 = new Professor.meuProprioConstrutor("Vitor", "Prado");
   print(p2.nomeCompleto);
+  //print(p2.displayName());
+
+  print("");
+  
+  ProfessorEmergencial p3 = new ProfessorEmergencial("Arthur", "Felipe");
+  print(p3.displayName());
+
+  print(ProfessorEmergencial.getVinculo());
+  
+  print("");
+  Pessoa pessoa = new PessoaHerdada();
+  pessoa.metodoAbstrato();
 }
