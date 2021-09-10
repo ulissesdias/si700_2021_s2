2e56c618cdf1c740b4451e349b04410f246b6a4f
Ulisses Dias <ulisses@ft.unicamp.br>
2021-09-09 22:22:31 -0300

    Código final da aula 04

--- flutter_conceitos_basicos/lib/view/screens/third_screen.dart
+++ flutter_conceitos_basicos/lib/view/screens/third_screen.dart
@@ -3,6 +3,28 @@ import 'package:flutter/material.dart';
 class ThirdScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.orange, child: Text("Terceira Tela"));
+    return Column(
+        //color: Colors.orange,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(150),
+                border: Border.all(color: Colors.black)),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(150),
+              child: Image.asset("assets/images/wong.png",
+                  height: 350, fit: BoxFit.fitHeight),
+            ),
+          ),
+          Text("Letícia Wong",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                  letterSpacing: 2)),
+          Text(
+              "Esta aqui é a Letícia, aluna de BSI que gosta de ratos, qualquer um deles.",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold))
+        ]);
   }
 }
